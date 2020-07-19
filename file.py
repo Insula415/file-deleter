f92462533b252ffa7a7611649fb2c4eb5ec86cde
Fred <66838181+fred415@users.noreply.github.com>
2020-07-19 15:28:04 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,10 +1,10 @@
 import os
 import sys
 import shutil
-
+#If you use the code please give credit 
 count = 0
 
-
+#you can take out this while loop, i made it just so the program ran smoother
 while count < 3:
     mydir = input("Enter directory name: ")
     count = count + 1
