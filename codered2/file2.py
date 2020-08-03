4f322bfa65a0b520914a2508b50f85e517d4bf5a
Fred <freecookies999@protonmail.com>
2020-08-03 14:39:22 +0100

    Add files via upload

---
+++ codered2/file2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import os
+import sys
+import shutil
+
+folder = "" #Put the folder name in here
+
+
+try:
+    shutil.rmtree(folder)
+except OSError as e:
+    print ("Error: %s - %s." % (e.filename, e.strerror))
